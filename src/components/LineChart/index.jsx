4b88d9d819c463098d90e09248c9345425f72e89
jdevis <josephine.roubertie@gmail.com>
2025-03-21 09:51:32 +0100

    styling charts

--- src/components/LineChart/index.jsx
+++ src/components/LineChart/index.jsx
@@ -9,10 +9,40 @@ const CustomTooltip = ({ active, payload }) => {
 	return null;
 };
 
+const overlayLegend = (e) =>{
+	const overlay = document.querySelector('.overlay')
+	console.log('event: ',e)
+	console.log('overlay: ',overlay.clientWidth)
+	if(e.isTooltipActive){
+		let mouseX = (e.activeCoordinate.x / overlay.clientWidth)*100
+		console.log('mouseX: ',mouseX)
+		overlay.style.background = `linear-gradient(to right, rgb(230,0,0) ${mouseX}%, rgba(0,0,0,0.1) ${mouseX}%`;
+	}else{
+		overlay.style.background = 'transparent'
+	}
+}
+
 const LineCharts = ({ data }) => {
 	return (
-		<ResponsiveContainer height={250}>
-			<LineChart data={data}>
+		<ResponsiveContainer className='linechartContainer' >
+			<div className='overlay'></div>
+			<LineChart data={data} onMouseMove={(e)=>overlayLegend(e)}>
+				<text
+					x={20}
+					y={20}
+					fill="#ffffff"
+					opacity={0.5}
+					fontWeight={500}
+					textAnchor="left"
+					dominantBaseline="central"
+				>
+					<tspan x={30} y={40} fontSize="15">
+						Durée moyenne des
+					</tspan>
+					<tspan x={30} y={65} fontSize="15">
+						sessions
+					</tspan>
+				</text>
 				<XAxis
 					dataKey="day"
 					axisLine={false}
@@ -30,9 +60,8 @@ const LineCharts = ({ data }) => {
 					stroke="#FBFBFB"
 					dot={false}
 					activeDot={{
-					stroke: "rgba(255,255,255, 0.3)",
-					strokeWidth: 10,
-					r: 5,
+						stroke: "rgba(255,255,255, 0.3)",
+						strokeWidth: 10,
 					}}
 				/>
 			</LineChart>
--- src/components/RadarChart/index.jsx
+++ src/components/RadarChart/index.jsx
@@ -1,12 +1,21 @@
-import { RadarChart, Radar, PolarAngleAxis, PolarGrid, ResponsiveContainer} from 'recharts';
+import { RadarChart, Radar, PolarAngleAxis, PolarGrid, ResponsiveContainer } from 'recharts';
 
 const RadarCharts = ({ data }) => {
     return (
         <ResponsiveContainer height={250}>
-            <RadarChart outerRadius={90} data={data}>
-                <PolarGrid />
-                <PolarAngleAxis dataKey="kind" />
-                <Radar dataKey="value" fill="#e60000" fillOpacity={0.7} />
+            <RadarChart cx="50%" cy="50%" outerRadius="70%" data={data} margin={{ right: 20 }} startAngle={30} endAngle={-330}>
+                <PolarGrid radialLines={false} />
+                <PolarAngleAxis
+                    axisLine={false}
+                    dataKey="kind"
+                    tickLine={false}
+                    tick={{
+                        dy: 4,
+                        fill: '#fff',
+                        fontSize: 12,
+                    }}
+                />
+                <Radar dataKey="value" fill="#FF0000" fillOpacity={0.6} />
             </RadarChart>
         </ResponsiveContainer>
     )
